020a7b56a6c16a45e64dac7704cc8e8a718ecb74
kazjon <kazjon.grace@sydney.edu.au>
2023-09-18 16:34:35 +1000

    Update.

--- scripts/parse_fsq.py
+++ scripts/parse_fsq.py
@@ -53,4 +53,4 @@ fields['survey_iteration'] = "INT"
 df["survey_iteration"] = None
 
 # Write the DataFrame to the database table
-df.to_sql(table_name, con=engine, if_exists='replace', index=False, dtype=fields)
+df.to_sql(table_name, con=engine, if_exists='replace', index=False)#, dtype=fields)
